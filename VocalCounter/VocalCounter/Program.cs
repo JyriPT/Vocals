815bbb5503365920b35dbf79be1e196ad5e59054
Jyri Turku <jyri.turku@student.saimia.fi>
2019-12-17 15:24:12 +0200

    Code fully functional and commented, used second method provided.

--- VocalCounter/VocalCounter/Program.cs
+++ VocalCounter/VocalCounter/Program.cs
@@ -15,14 +15,35 @@ namespace VocalCounter
             Console.WriteLine($"Output: Your input, ¨{DATA.ToUpper()}¨, contains {count(DATA)} vowels.");
         }
 
-        static int count (string input)
+        //Vokaalien laskenta (vaihtoehto 2)
+        static int count (string DATA)
         {
+            //Määritellään vokaalit, luodaan muuttuja syötteen käsittelyä varten
             string vowels = "AEIOUYÄÖ";
-            string data = input.ToUpper();
-            int num = 0;
+            string process = DATA.ToUpper();
 
+            //Käydään jokainen vokaali läpi ja poistetaan ne muokatusta syötteestä
+            foreach (char vowel in vowels)
+            {
+                //Tarkistaa löytyykö kyseistä vokaalia, jos löytyy niin poistaa ja tarkistaa uudelleen
+                int check = process.IndexOf(vowel);
 
+                if (check >= 0)
+                {
+                    do
+                    {
+                        process = process.Remove(check, 1);
 
+                        check = process.IndexOf(vowel);
+
+                    } while (check >= 0);
+                }
+                
+            }
+
+            //Lasketaan vokaalien määrä (alkuperäisen syötteen pituus - muokatun lauseen pituus)
+            int num = DATA.Length - process.Length;
+            //Palautetaan tulos
             return num;
         }
     }
