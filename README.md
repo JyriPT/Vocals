21e5a6d55f0c028171e5b1d9ced2a76eede2deee
Jyri Turku <jyri.turku@student.saimia.fi>
2019-12-17 14:45:24 +0200

    Basic structure of the program is up, now to decide on actual functionality.
    Also a small update to the README to address an inconsistency in the naming of the project.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Vocals
+
+I realized halfway through it is meant to be "vowels". I know, I am and idiot.
--- VocalCounter/VocalCounter/Program.cs
+++ VocalCounter/VocalCounter/Program.cs
@@ -6,7 +6,24 @@ namespace VocalCounter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Pyydetään käyttäjältä syöttö tarkistettavaksi
+            Console.WriteLine("Please provide an input:");
+            string DATA = Console.ReadLine();
+
+            //Tulostetaan vastaukset, tarkastetaan vokaalien määrä tulostuksen yhteydessä.
+            Console.WriteLine($"Input: {DATA}");
+            Console.WriteLine($"Output: Your input, ¨{DATA.ToUpper()}¨, contains {count(DATA)} vowels.");
+        }
+
+        static int count (string input)
+        {
+            string vowels = "AEIOUYÄÖ";
+            string data = input.ToUpper();
+            int num = 0;
+
+
+
+            return num;
         }
     }
 }
